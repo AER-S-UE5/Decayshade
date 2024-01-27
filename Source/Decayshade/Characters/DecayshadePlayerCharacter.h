b31b8a036de472a5936cff8efad5acf3a8867a69
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-01-27 23:52:06 +0100

    Creating the DayshadePlayerCharacter class

---
+++ Source/Decayshade/Characters/DecayshadePlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DecayshadeCharacter.h"
+#include "DecayshadePlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class DECAYSHADE_API ADecayshadePlayerCharacter : public ADecayshadeCharacter
+{
+	GENERATED_BODY()
+	
+};
